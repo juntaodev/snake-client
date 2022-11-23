3715c79a8236ac64544c5b788594a554c048e863
juntaodev <juntaoliudev@gmail.com>
2022-11-23 17:03:54 -0500

    refactor input module

--- play.js
+++ play.js
@@ -1,24 +1,8 @@
 const {connect} = require('./client');
+const {setupInput} = require('./input');
 
-// setup interface to handle user input from stdin
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
 
-  const handleUserInput = function() {
-    stdin.on('data', (key) => {
-      if (key === '\u0003') {
-        process.exit();
-      }
-    });
-  };
- 
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
+console.log('Connecting ...');
 
 connect();
 setupInput();
