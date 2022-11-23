15662154ba3415298c3ec43f05d1e8b592a5ec96
juntaodev <juntaoliudev@gmail.com>
2022-11-23 16:36:02 -0500

    refactor

--- play.js
+++ play.js
@@ -1,20 +1,6 @@
-const net = require('net');
+const {connect} = require('./client');
 
-const config = {
-  // host: 'localhost', // '127.0.0.1'
-  // port: 5000
-  host: '6.tcp.ngrok.io',
-  port: 11872
-};
 
-const client = net.createConnection(config);
 
-client.setEncoding('utf-8');
 
-client.on('data', (data) => {
-  console.log(data);
-});
-
-process.stdin.on('data', (userInput) => {
-  client.write(userInput);
-});
+connect();
