2668cf4d03eecc86a7744300195492f3f372354a
juntaodev <juntaoliudev@gmail.com>
2022-11-23 21:22:54 -0500

    add WASD movement and refactor

--- client.js
+++ client.js
@@ -3,18 +3,16 @@ const net = require("net");
 // establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
-    host: '6.tcp.ngrok.io',// IP address here,
-    port: 11872// PORT number here,
+    host: '165.227.47.243',// IP address here,
+    port: 50541// PORT number here,
   });
-  // interpret incoming data as text
+  
   conn.setEncoding("utf8");
 
   conn.on("connect", () => {
     console.log("Connected to game server");
     conn.write("Name: JTL");
-    setTimeout(() => {
-      conn.write("Move: up");
-    }, 2000);
+    
   });
 
   return conn;
--- input.js
+++ input.js
@@ -1,5 +1,9 @@
-// setup interface to handle user input from stdin
-const setupInput = function() {
+
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -9,6 +13,19 @@ const setupInput = function() {
     stdin.on('data', (key) => {
       if (key === '\u0003') {
         process.exit();
+      }
+
+      if (key === 'w') {
+        connection.write('Move: up');
+      }
+      if (key === 'a') {
+        connection.write('Move: left');
+      }
+      if (key === 's') {
+        connection.write('Move: down');
+      }
+      if (key === 'd') {
+        connection.write('Move: right');
       }
     });
   };
